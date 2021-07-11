d484b0e32f607ad51294520cb38ddad7327f34f0
Gouri.k <gk0309002@gmail.com>
2021-07-11 23:03:37 +0530

    Initial commit

---
+++ js/templatemo-script.js
@@ -0,0 +1,67 @@
+const initBg = (autoplay = true) => {
+    const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    const bgImgs = bgImgsNames.map(img => "img/" + img);
+
+    $.backstretch(bgImgs, {duration: 4000, fade: 500});
+
+    if(!autoplay) {
+      $.backstretch('pause');  
+    }    
+}
+
+const setBg = id => {
+    $.backstretch('show', id);
+}
+
+const setBgOverlay = () => {
+    const windowWidth = window.innerWidth;
+    const bgHeight = $('body').height();
+    const tmBgLeft = $('.tm-bg-left');
+
+    $('.tm-bg').height(bgHeight);
+
+    if(windowWidth > 768) {
+        tmBgLeft.css('border-left', `0`)
+                .css('border-top', `${bgHeight}px solid transparent`);                
+    } else {
+        tmBgLeft.css('border-left', `${windowWidth}px solid transparent`)
+                .css('border-top', `0`);
+    }
+}
+
+$(document).ready(function () {
+    const autoplayBg = true;	// set Auto Play for Background Images
+    initBg(autoplayBg);    
+    setBgOverlay();
+
+    const bgControl = $('.tm-bg-control');            
+    bgControl.click(function() {
+        bgControl.removeClass('active');
+        $(this).addClass('active');
+        const id = $(this).data('id');                
+        setBg(id);
+    });
+
+    $(window).on("backstretch.after", function (e, instance, index) {        
+        const bgControl = $('.tm-bg-control');
+        bgControl.removeClass('active');
+        const current = $(".tm-bg-controls-wrapper").find(`[data-id=${index}]`);        
+        current.addClass('active');
+    });
+
+    $(window).resize(function() {
+        setBgOverlay();
+    });
+});
+function setProgress(elem, percent) {
+    var
+      degrees = percent * 3.6,
+      transform = /MSIE 9/.test(navigator.userAgent) ? 'msTransform' : 'transform';
+    elem.querySelector('.counter').setAttribute('data-percent', Math.round(percent));
+    elem.querySelector('.progressEnd').style[transform] = 'rotate(' + degrees + 'deg)';
+    elem.querySelector('.progress').style[transform] = 'rotate(' + degrees + 'deg)';
+    if(percent >= 50 && !/(^|\s)fiftyPlus(\s|$)/.test(elem.className))
+      elem.className += ' fiftyPlus';
+  }
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Message sent"; }
